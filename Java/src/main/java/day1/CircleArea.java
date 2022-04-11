2bfa83f79a804e0caa389d3132be59a2ed9bd8f7
jinuk93 <rlawlsdnr03@naver.com>
2022-04-11 17:01:53 +0900

    [추가] Java

--- Java/src/main/java/day1/CircleArea.java
+++ Java/src/main/java/day1/CircleArea.java
@@ -7,12 +7,13 @@ package day1;
 public class CircleArea {
 
 	public static void main(String[] args) {
-		final double PI = 3.14;
+		final double PI = 3.14; //상수명은 대문자로 만든다
 		double radius = 10.2;
 		double circleArea = radius*radius*PI;
 		
 		//원의 면적을 화면에 출력한다
-		System.out.println();
-		System.out.println();
+		System.out.print("반지름 " + radius + " ,");
+		System.out.println(" 원의면적 "  + circleArea);
+		
 	}
 }
