1d4245a31d1753f78437aa2bf0cb9267c3ea3353
jinuk93 <rlawlsdnr03@naver.com>
2022-04-11 19:06:17 +0900

    [추가] Java 1day

---
+++ Java/src/main/java/day1/CircleArea2.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class CircleArea2 {
+
+	public static void main(String[] args) {
+		int x=2, y=10, z=0;
+		
+		z=x++*2+--y-5+x*(y%2);
+		
+		System.out.println("z = " +z);
+		System.out.println(8>>1);
+		
+		int opr = 4;
+		System.out.println(opr++);
+		System.out.println(opr);
+
+	}
+}
