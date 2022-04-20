2f539cb5714bdcc4fe35c4176783359a408f632b
jinuk93 <rlawlsdnr03@naver.com>
2022-04-20 15:14:20 +0900

    [추가] sql

--- Db/lec/26.GROUP함수.sql
+++ Db/lec/26.GROUP함수.sql
@@ -14,19 +14,31 @@ VARIANCE 분산을 계산
 3) GROUP BY 없이 일반 컬럼과 기술될 수 없다
 
 1) 사원의 평균 급여를 검색한다
+SELECT sal
+ FROM emp;
 
-
-
+SELECT AVG(sal), ROUND(AVG(sal)) 
+ FROM emp;
+ 
 2) 사원들에게 지급된 보너스 총합과 보너스 평균을 검색한다
-SELECT SUM(comm)               "보너스 총합",
-       ROUND(AVG(comm))        "보너스 평균",
-       COUNT(comm)             "수령 인원",
-       ROUND(AVG(NVL(comm,0))) "환산 평균",
-       COUNT(*)                "전체 인원"
-  FROM emp;
+SELECT SUM(comm) "보너스 총합",
+       ROUND(AVG(comm)) "보너스 평균",
+       COUNT(comm)  "수령 인원",
+       ROUND(AVG(NVL(comm,0)))  "환산 평균",
+       COUNT(*)     "전체 인원"
+ FROM emp;
 
 3) 보너스에서 null 이 아닌 사람 수를 계산하세요
-
+SELECT COUNT(*)-COUNT(comm)
+ FROM emp;
+ 
+SELECT COUNT(*)
+ FROM emp
+ WHERE comm IS NULL;
+ 
+SELECT COUNT(*)
+ FROM emp
+ WHERE comm IS NOT NULL;
 
 
 그룹 함수와 GROUP BY 절
@@ -46,20 +58,38 @@ ORDER BY 정렬대상;
 --ORA-00937 : 카디널리티가 일치하지 않는다
 --AVG 함수는 1개의 결과값
 --job 은 행개수만큼 결과값
-
-
-
+SELECT job 업무, ROUND(AVG(sal)) 평균급여,
+       ROUND(AVG(sal*12+NVL(comm,0))) 평균연봉
+ FROM emp;
+ 
 결과 행의 개수가 14개이다
-
+SELECT job 업무
+ FROM emp;
 
 결과 행의 개수가 1개이다(그룹함수)
-
+SELECT ROUND(AVG(sal)) 평균급여,
+       ROUND(AVG(sal*12+NVL(comm,0))) 평균연봉
+ FROM emp;
 
 * 그룹 함수와 함께 사용되는 일반 컬럼은
 반드시 GROUP BY 에 기술되어야 한다
 
+SELECT job 업무, ROUND(AVG(sal)) 평균급여,
+       ROUND(AVG(sal*12+NVL(comm,0))) 평균연봉
+ FROM emp
+ GROUP BY job;
 
 5) 부서별 평균 급여, 평균 연봉을 검색한다
+SELECT dno, ROUND(AVG(sal)),
+       ROUND(AVG(sal*12+NVL(comm,0)))
+ FROM emp
+ GROUP BY dno;
+ 
+SELECT dno, dname, ROUND(AVG(sal)),
+       ROUND(AVG(sal*12+NVL(comm,0)))
+ FROM emp
+ JOIN dept USING(dno)
+ GROUP BY dno, dname;
 
 
 --TO_CHAR(AVG(result), '99.99') "기말 평균"
