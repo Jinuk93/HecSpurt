9c799a0e06690275c420c84c775c2532d6f6f8ac
jinuk93 <rlawlsdnr03@naver.com>
2022-05-20 10:05:44 +0900

    추가 및 수정

---
+++ Spring/src/main/java/org/conan/domain/SampleDTOList.java
@@ -0,0 +1,13 @@
+package org.conan.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class SampleDTOList {
+	private List<SampleDTO> list;
+	private SampleDTOList() {
+		list = new ArrayList<>();
+	}
+}
---
+++ Spring/src/test/java/org/conan/persistence/BoardServiceTest.java
@@ -0,0 +1,59 @@
+package org.conan.persistence;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import org.conan.config.RootConfig;
+import org.conan.domain.BoardVO;
+import org.conan.servcie.BoardService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {RootConfig.class})
+@Log4j
+public class BoardServiceTest {
+	@Setter(onMethod_= {@Autowired})
+	private BoardService service;
+	
+	@Test
+	public void testExist() {
+		log.info(service);
+		assertNotNull(service);
+	}
+	@Test
+	public void testGetList() {
+		service.getList().forEach(board-> log.info(board));
+	}
+	@Test
+	public void testRegister() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새글 새글 새글 from service");
+		board.setContent("새 내용 새 내용 from servcie");
+		board.setWriter("newbie");
+		service.register(board);
+		log.info("생성된 게시글의 번호 : " + board.getBno());
+	}
+	@Test
+	public void testGet() {
+		log.info(service.get(6L).getTitle());
+	}
+	@Test
+	public void testDelete() {
+		log.info("REMOVE Result : " + service.remove(6L));
+	}
+	@Test
+	public void testUpdate() {
+		BoardVO board = service.get(6L);
+		if(board == null) {return;}
+		board.setTitle("제목 수정 from Serivce");
+		log.info("MODIFY RESULT : " + service.modify(board));
+	}
+}
